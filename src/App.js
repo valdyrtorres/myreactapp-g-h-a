cacdaf537373482d76a999eba842905a0d6e0091
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2023-11-06 00:20:12 -0300

    Update App.js 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Valdir Torres
+          Learn React with Valdir Torres B
         </a>
       </header>
     </div>
